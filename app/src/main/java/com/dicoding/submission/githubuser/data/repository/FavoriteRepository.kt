ca4b5d3101cf0b11ae07ceb2fa6260847fb1ddf6
muham <muhammadhassan7788@gmail.com>
2021-09-13 21:08:10 +0700

    Add Coroutine to Room Database

--- app/src/main/java/com/dicoding/submission/githubuser/data/repository/FavoriteRepository.kt
+++ app/src/main/java/com/dicoding/submission/githubuser/data/repository/FavoriteRepository.kt
@@ -7,18 +7,25 @@
 package com.dicoding.submission.githubuser.data.repository
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.dicoding.submission.githubuser.data.database.GithubDatabase
 import com.dicoding.submission.githubuser.data.database.dao.FavoriteDao
 import com.dicoding.submission.githubuser.data.database.entity.Favorite
 import com.dicoding.submission.githubuser.others.ApiResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class FavoriteRepository(application: Application) {
+class FavoriteRepository(var application: Application) {
     private var favoriteDao: FavoriteDao
     private var database = GithubDatabase.getInstance(application)
     private var data = MutableLiveData<ApiResponse<List<Favorite>>>()
     private var singleData = MutableLiveData<ApiResponse<Favorite>>()
+
+    companion object {
+        private const val TAG = "FavoriteRepository"
+    }
 
     fun getData(): MutableLiveData<ApiResponse<List<Favorite>>> = data
 
@@ -29,21 +36,21 @@ class FavoriteRepository(application: Application) {
         data.value = ApiResponse.noData()
     }
 
-    fun insert(favorite: Favorite) {
-        favoriteDao.insert(favorite)
-        getAll()
-    }
-
-    fun delete(favorite: Favorite) {
-        favoriteDao.delete(favorite)
-        getAll()
+    suspend fun insert(favorite: Favorite) {
+        withContext(Dispatchers.IO) {
+            favorite.username?.let {
+                Log.i(TAG, "insert: $favorite")
+                favoriteDao.insert(favorite)
+                getByUsername(it)
+            }
+        }
     }
 
     fun getAll(): MutableLiveData<ApiResponse<List<Favorite>>> {
         val result = favoriteDao.getAll()
         if (result.value != null && result.value!!.isNotEmpty()) {
-            data.value = ApiResponse.success(result.value)
-        } else data.value = ApiResponse.noData()
+            data.postValue(ApiResponse.success(result.value))
+        } else data.postValue(ApiResponse.noData())
 
         return data
     }
@@ -53,12 +60,22 @@ class FavoriteRepository(application: Application) {
         getAll()
     }
 
-    fun getByUsername(username: String): LiveData<ApiResponse<Favorite>> {
-        val result = favoriteDao.findByUsername(username)
-        if (result.value != null) {
-            singleData.value = ApiResponse.success(result.value)
-        } else data.value = ApiResponse.noData()
-
+    suspend fun getByUsername(username: String): LiveData<ApiResponse<Favorite>> {
+        withContext(Dispatchers.IO) {
+            val result = favoriteDao.findByUsername(username)
+            Log.i(TAG, "getByUsername: $username result = ${result}")
+            if (result != null) {
+                singleData.postValue(ApiResponse.success(result))
+            } else singleData.postValue(ApiResponse.noData())
+        }
         return singleData
+    }
+
+    suspend fun deleteByusername(username: String) {
+        withContext(Dispatchers.IO) {
+            Log.i(TAG, "deleteByusername: $username")
+            favoriteDao.deleteByUsername(username)
+            getByUsername(username)
+        }
     }
 }
