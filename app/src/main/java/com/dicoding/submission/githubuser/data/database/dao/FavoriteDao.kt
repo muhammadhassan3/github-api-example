7b5710ed0933675b79a2d3e5016461771c64a7d4
muham <muhammadhassan7788@gmail.com>
2021-09-13 21:07:42 +0700

    - Fix findByUsername Function - Add deleteByUsername Function

--- app/src/main/java/com/dicoding/submission/githubuser/data/database/dao/FavoriteDao.kt
+++ app/src/main/java/com/dicoding/submission/githubuser/data/database/dao/FavoriteDao.kt
@@ -8,7 +8,6 @@ package com.dicoding.submission.githubuser.data.database.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.dicoding.submission.githubuser.data.database.entity.Favorite
@@ -19,8 +18,8 @@ interface FavoriteDao {
     @Insert
     fun insert(favorite: Favorite)
 
-    @Delete
-    fun delete(favorite: Favorite)
+    @Query("delete from tbl_favorite  where username = :username")
+    fun deleteByUsername(username: String)
 
     @Query("delete from tbl_favorite")
     fun deleteAll()
@@ -28,6 +27,6 @@ interface FavoriteDao {
     @Query("select * from tbl_favorite order by id desc")
     fun getAll(): LiveData<List<Favorite>>
 
-    @Query("select * from tbl_favorite e where e.username = :username limit 1")
-    fun findByUsername(username: String): LiveData<Favorite>
+    @Query("select * from tbl_favorite e where e.username = :username")
+    fun findByUsername(username: String): Favorite
 }
